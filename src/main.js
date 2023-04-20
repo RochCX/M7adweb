a946ee1f5061559c47ef3e2ea3c70aa1941c0438
RochCX <103058028+RochCX@users.noreply.github.com>
2023-04-20 11:16:30 -0400

    Peque (#2)
    * bootstrap fortawesome
    
    * sweetalert2

--- src/main.js
+++ src/main.js
@@ -2,5 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-createApp(App).use(store).use(router).mount('#app')
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(fas)
+
+// bootstrap 5.3
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+
+createApp(App).use(store).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,23 @@
 <template>
   <h1>Inicio de Sesión</h1>
+  <div class="container">
+    <div class="row my-5 justify-content-center">
+      <div class="col-md-6 my-5 ">
+        <div class="card shadow-lg">
+          <div class="card-header bg-success text-white">
+            <p class="h3">Ingreso</p>
+          </div>
+          <div class="card-body bg-light">
+            <font-awesome-icon :icon="['fas', 'trash']" />
+            <!-- <form @submit.prevent="validarLogin({usuario: user.email, contrasena: user.password, usuarios: usuarios})">
+                <input v-model="user.email"  class="form-control mt-3" placeholder="Email" type="email">
+                <input v-model="user.password"  class="form-control mt-3" placeholder="Password" type="password">
+                <input class="btn btn-success mt-3" type="submit" value="Ingresar">
+            </form>
+            <div v-if="!loggin" class="mt-4">{{mensajeError}}</div> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
