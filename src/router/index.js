bd9bac63a0197376e91d07472e81f1dfdc2315f4
RochCX <103058028+RochCX@users.noreply.github.com>
2023-04-24 09:46:19 -0400

    Viernes (#7)
    * intento de redireccion
    
    * redireccionador con alert funcionando
    
    * redireccionado (sin localstorage)

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import RegisterView from '../views/RegisterView.vue'
 import LoginView from '../views/LoginView.vue'
 import HomeView from '../views/HomeView.vue'
 import EditarCursos from '../views/EditarCursos.vue'
+import store from "@/store";
+import Swal from 'sweetalert2'
+const state = store.state;
 
 const routes = [
   {
@@ -31,5 +34,38 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to,from,next) => {
+  if (to.path === '/' || to.path === '/register') {
+    next();
+  }
+    else if(state.conectado == false){
+      let timerInterval
+                        Swal.fire({
+                        title: 'Debes conectarte primero!',
+                        html: 'Seras redireccionado en <b></b> milisegundos.',
+                        timer: 3500,
+                        timerProgressBar: true,
+                        didOpen: () => {
+                            Swal.showLoading()
+                            const b = Swal.getHtmlContainer().querySelector('b')
+                            timerInterval = setInterval(() => {
+                            b.textContent = Swal.getTimerLeft()
+                            }, 100)
+                        },
+                        willClose: () => {
+                            clearInterval(timerInterval)
+                        }
+                        }).then((result) => {
+                            next('/')
+                        /* Read more about handling dismissals below */
+                        if (result.dismiss === Swal.DismissReason.timer) {
+                            console.log('Fui cerrado por el timer')
+                        }
+                        })
+    } else{
+      next();
+  }
+});
 
 export default router
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,48 @@
 </template>
 
 <script>
+import store from "@/store";
+import Swal from 'sweetalert2'
+import router from "@/router";
+
+const state = store.state;
+
     import CursosCards from '@/components/CursosCards.vue'
     export default {
         components: {
             CursosCards
-        }
+        },
+        computed: {
+            
+        },
+        methods:{
+            arranca(){
+                if (state.conectado == false) {
+                    let timerInterval
+                        Swal.fire({
+                        title: 'Auto close alert!',
+                        html: 'I will close in <b></b> milliseconds.',
+                        timer: 2000,
+                        timerProgressBar: true,
+                        didOpen: () => {
+                            Swal.showLoading()
+                            const b = Swal.getHtmlContainer().querySelector('b')
+                            timerInterval = setInterval(() => {
+                            b.textContent = Swal.getTimerLeft()
+                            }, 100)
+                        },
+                        willClose: () => {
+                            clearInterval(timerInterval)
+                        }
+                        }).then((result) => {
+                            router.push('/')
+                        /* Read more about handling dismissals below */
+                        if (result.dismiss === Swal.DismissReason.timer) {
+                            console.log('I was closed by the timer')
+                        }
+                        })
+            }
+            }
+        },
     }
 </script>
