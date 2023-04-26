6f4488a445532a99fdf1f2c70e82ea5bcc7f07b8
ManuDv <m.alfaro@live.cl>
2023-04-26 13:46:53 -0400

    Cambia funciones hardcoded del CRUD a mutaciones y acciones dentro del Store

--- src/views/EditarCursos.vue
+++ src/views/EditarCursos.vue
@@ -7,6 +7,7 @@
             <tr>
                 <th scope="col">Nombre</th>
                 <th scope="col">Descripcion</th>
+                <th scope="col">Código</th>
                 <th scope="col">Precio</th>
                 <th scope="col">Duracion</th>
                 <th scope="col">Cupos</th>
@@ -16,13 +17,12 @@
                 <th scope="col">Eliminar</th>
             </tr>
         </thead>
-        <div v-if="arrayCursos.length == 0" id="carga">
-            <PulseLoader></PulseLoader>
-        </div>
-        <tbody v-else v-for="curso in arrayCursos" :key="curso.codigo">
+
+        <tbody v-for="curso in arrayCursos" :key="curso.codigo">
             <tr>
                 <td>{{ curso.nombre }}</td>
                 <td>{{ curso.descripcion }}</td>
+                <td>{{ curso.codigo }}</td>
                 <td>${{ parseInt(curso.precio).toLocaleString('es-cl') }}</td>
                 <td>{{ curso.duracion }}</td>
                 <td>{{ curso.cupos }}</td>
@@ -34,6 +34,9 @@
             </tr>
         </tbody>
     </table>
+    <div v-if="arrayCursos.length == 0" id="carga">
+        <PulseLoader></PulseLoader>
+    </div>
 
     <!-- Modal -->
     <div class="modal fade" id="curso-nuevo" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -50,6 +53,8 @@
                         v-model="datosModal.inputImg">
                     <input type="text" class="form-control my-3" placeholder="Descripción del Curso"
                         v-model="datosModal.inputDescripcion">
+                    <input type="text" class="form-control my-3" placeholder="Código del Curso"
+                        v-model="datosModal.inputCodigo">
                     <input type="text" class="form-control my-3" placeholder="Precio del Curso"
                         v-model="datosModal.inputPrecio">
                     <input type="text" class="form-control my-3" placeholder="Duracion del Curso"
@@ -61,17 +66,18 @@
                     <div class="form-check form-switch">
                         <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"
                             v-model="datosModal.inputEstado">
-                        <label class="form-check-label" for="flexSwitchCheckDefault">{{ datosModal.inputEstado ? "Disponible"
+                        <label class="form-check-label" for="flexSwitchCheckDefault">{{ datosModal.inputEstado ?
+                            "Disponible"
                             : "No Disponible" }}</label>
                     </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
                     <button v-if="headerModal == 'Crear Curso'" type="button" class="btn btn-primary"
-                        data-bs-dismiss="modal" @click="agregarCurso">
+                        data-bs-dismiss="modal" @click="agregarCurso(datosModal)">
                         {{ headerModal }}</button>
-                    <button v-else type="button" class="btn btn-primary" data-bs-dismiss="modal"
-                        @click="modificarCurso">{{ headerModal }}</button>
+                    <button v-else type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="modificarCurso(datosModal)">{{
+                        headerModal }}</button>
                 </div>
             </div>
         </div>
@@ -79,9 +85,9 @@
 </template>
 
 <script>
-import {mapGetters, mapActions} from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 import { db } from '@/firebase/index.js'
-import { collection, getDocs, doc, deleteDoc, addDoc, setDoc } from "firebase/firestore";
+import { getDocs} from "firebase/firestore";
 import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 import Swal from 'sweetalert2'
 
@@ -94,16 +100,17 @@ export default {
             cursos: [],
             cargando: false,
             headerModal: "Crear Curso",
-            cursoEdit: {},
             datosModal: {
                 inputNombre: "",
                 inputDescripcion: "",
+                inputCodigo: "",
                 inputPrecio: "",
                 inputDuracion: "",
                 inputCupos: "",
                 inputInscritos: "",
                 inputImg: "https://placehold.co/600x400",
                 inputEstado: false,
+                cursoID: "",
             },
 
         }
@@ -116,16 +123,11 @@ export default {
         ...mapGetters(['arrayCursos']),
     },
     methods: {
-        ...mapActions(['cargarCursos']),
+        ...mapActions(['cargarCursos', 'eliminarCurso', 'agregarCurso', 'modificarCurso']),
 
         async retornarAlgo(elemento) {
             await console.log(elemento)
             console.log(getDocs(db, "cursos").data())
-        },
-
-        async eliminarCurso(elemento) {
-            await deleteDoc(doc(db, "cursos", elemento))
-            this.cargarCursos();
         },
 
         modalBorrar(laID) {
@@ -135,7 +137,8 @@ export default {
                 confirmButtonText: 'Eliminar',
             }).then(result => {
                 if (result.isConfirmed) {
-                    this.eliminarCurso(laID)
+                    this.eliminarCurso(laID);
+                    this.cargarCursos()
                     Swal.fire('Eliminado!', '', 'success')
                 } else if (result.isDenied) {
                     Swal.fire('Acción cancelada', '', 'info')
@@ -150,70 +153,33 @@ export default {
 
         vaciarCurso() {
             this.datosModal.inputNombre = "",
-                this.datosModal.inputDescripcion = "",
-                this.datosModal.inputPrecio = "",
-                this.datosModal.inputDuracion = "",
-                this.datosModal.inputCupos = "",
-                this.datosModal.inputInscritos = "",
-                this.datosModal.inputEstado = "",
-                this.datosModal.inputImg = ""
+            this.datosModal.inputImg = "https://placehold.co/600x400",
+            this.datosModal.inputDescripcion = "",
+            this.datosModal.inputCodigo = "",
+            this.datosModal.inputPrecio = "",
+            this.datosModal.inputDuracion = "",
+            this.datosModal.inputCupos = "",
+            this.datosModal.inputInscritos = "",
+            this.datosModal.inputEstado = ""
         },
 
         editarCurso(objeto) {
             this.headerModal = "Editar Curso",
                 this.datosModal.inputNombre = objeto.nombre,
                 this.datosModal.inputDescripcion = objeto.descripcion,
+                this.datosModal.inputCodigo = objeto.codigo,
                 this.datosModal.inputPrecio = objeto.precio,
                 this.datosModal.inputDuracion = objeto.duracion,
                 this.datosModal.inputCupos = objeto.cupos,
                 this.datosModal.inputInscritos = objeto.inscritos,
                 this.datosModal.inputEstado = objeto.estado,
                 this.datosModal.inputImg = objeto.img
-            this.cursoEdit = objeto;
+                this.datosModal.cursoID = objeto.id;
         },
 
-        async modificarCurso() {
-            await setDoc(doc(db, "cursos", this.cursoEdit.id), {
-                id: this.cursoEdit.id,
-                nombre: this.datosModal.inputNombre,
-                descripcion: this.datosModal.inputDescripcion,
-                precio: this.datosModal.inputPrecio,
-                duracion: this.datosModal.inputDuracion,
-                cupos: this.datosModal.inputCupos,
-                inscritos: this.datosModal.inputInscritos,
-                estado: this.datosModal.inputEstado,
-                img: this.datosModal.inputImg
-            });
-            Swal.fire({
-                title: '¡Editado!',
-                text: 'El curso se ha editado correctamente',
-                icon: 'success',
-            });
-            this.cargarCursos()
-        },
-
-        async agregarCurso() {
-            const docRef = await addDoc(collection(db, "cursos"), {});
-            docRef;
-            await setDoc(doc(db, "cursos", docRef.id), {
-                id: docRef.id,
-                nombre: this.datosModal.inputNombre,
-                descripcion: this.datosModal.inputDescripcion,
-                precio: this.datosModal.inputPrecio,
-                duracion: this.datosModal.inputDuracion,
-                cupos: this.datosModal.inputCupos,
-                inscritos: this.datosModal.inputInscritos,
-                estado: this.datosModal.inputEstado,
-                img: this.datosModal.inputImg
-            });
-            Swal.fire({
-                title: '¡Creado!',
-                text: 'El curso se ha creado correctamente',
-                icon: 'success',
-            });
-            this.cargarCursos()
-        }
+        
     },
+
     created: async function () {
         this.cargarCursos()
     }
@@ -231,4 +197,5 @@ export default {
 
 .rojo {
     color: red;
-}</style>
+}
+</style>
