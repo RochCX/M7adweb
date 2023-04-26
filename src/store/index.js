3887d5314910ddeda0935c7b0036db31d6cdfdfc
Manuel Alfaro <m.alfaro@live.cl>
2023-04-26 13:51:59 -0400

    Cambia funciones hardcoded del CRUD a mutaciones y acciones dentro del Store (#15)

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore } from 'vuex'
 import { db } from '@/firebase/index.js'
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, doc, addDoc, deleteDoc, setDoc } from "firebase/firestore";
+import Swal from 'sweetalert2';
 import { getAuth, signOut } from "firebase/auth";
 import router from "@/router";
 const auth = getAuth();
@@ -43,7 +44,55 @@ export default createStore({
     async cargarCursos({commit}) {
       const querySnapshot = await getDocs(collection(db, "cursos"));  
       commit('cargarCursos', querySnapshot)
-    }
+    },
+    async eliminarCurso(state, elemento) {
+      await deleteDoc(doc(db, "cursos", elemento));
+    },
+    async agregarCurso({dispatch}, payload) {
+      const objeto = {...payload};
+      const docRef = await addDoc(collection(db, "cursos"), {});
+      docRef;
+      await setDoc(doc(db, "cursos", docRef.id), {
+          id: docRef.id,
+          nombre: objeto.inputNombre,
+          descripcion: objeto.inputDescripcion,
+          codigo: objeto.inputCodigo,
+          precio: objeto.inputPrecio,
+          duracion: objeto.inputDuracion,
+          cupos: objeto.inputCupos,
+          inscritos: objeto.inputInscritos,
+          estado: objeto.inputEstado,
+          img: objeto.inputImg
+      });
+      Swal.fire({
+          title: '¡Creado!',
+          text: 'El curso se ha creado correctamente',
+          icon: 'success',
+      });
+      dispatch('cargarCursos')
+    },
+    async modificarCurso({dispatch}, payload) {
+          const objeto = {...payload};
+          console.log(objeto)
+       await setDoc(doc(db, "cursos", objeto.cursoID), {
+          id: objeto.cursoID,
+          nombre: objeto.inputNombre,
+          descripcion: objeto.inputDescripcion,
+          codigo: objeto.inputCodigo,
+          precio: objeto.inputPrecio,
+          duracion: objeto.inputDuracion,
+          cupos: objeto.inputCupos,
+          inscritos: objeto.inputInscritos,
+          estado: objeto.inputEstado,
+          img: objeto.inputImg
+      });
+      Swal.fire({
+          title: '¡Editado!',
+          text: 'El curso se ha editado correctamente',
+          icon: 'success',
+      }); 
+      dispatch('cargarCursos')
+  },
   },
   modules: {
   }
