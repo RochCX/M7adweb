35e8fc8b3edbf089d953534375f4a5c210b313e6
Manuel Alfaro <m.alfaro@live.cl>
2023-04-27 12:57:41 -0400

    Importante (#21)
    * cambia favicon y titulo
    
    * probando
    
    * Arregla redireccionamiento next y animacion de flop

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ const auth = getAuth();
 
 const state = store.state;
 
+
 const routes = [
   {
     path: '/home',
@@ -39,7 +40,67 @@ const router = createRouter({
   routes
 })
 
+
 router.beforeEach((to,from,next) => {
+
+  let estadoConexion = eval(localStorage.getItem("conexion"))
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      state.conectado = true
+      localStorage.setItem("conexion", state.conectado);
+      state.fireMail = user.email;
+      return
+    } else {
+      state.conectado = false;
+      state.fireMail = null;
+      localStorage.setItem("conexion", state.conectado);
+      return
+    }
+  })
+
+  // Ya esta logueado
+  if(estadoConexion && to.path == '/') {
+    Swal.fire("¡Ya estás conectado!")
+        next('/home')
+        return
+  }
+
+  // Debes conectarte primero
+  if(!estadoConexion) {
+    if (to.path == '/register') {
+      next()
+      return
+    } else if (to.path != '/') {
+      let timerInterval
+          Swal.fire({
+          title: 'Debes conectarte primero!',
+          html: 'Serás redireccionado en <b></b> segundos.',
+          timer: 3500,
+          timerProgressBar: true,
+          didOpen: () => {
+              Swal.showLoading()
+              const b = Swal.getHtmlContainer().querySelector('b')
+              timerInterval = setInterval(() => {
+              b.textContent = (Swal.getTimerLeft()/1000).toFixed(0)
+              }, 100)
+          },
+          willClose: () => {
+              clearInterval(timerInterval)
+          }
+          }).then((result) => {
+              next('/')
+          if (result.dismiss === Swal.DismissReason.timer) {
+              console.log('Fui cerrado por el timer')
+          }
+          })
+      return
+    } 
+  } 
+  next();
+})
+
+/* router.beforeEach((to,from,next) => {
   onAuthStateChanged(auth, (user) => {
     if (user) {
       // User is signed in, see docs for a list of available properties
@@ -48,7 +109,7 @@ router.beforeEach((to,from,next) => {
       console.log(uid);
       state.conectado = true;
       console.log(state.conectado);
-      state.fireMail = user.email
+      
       console.log(state.fireMail);
       if(to.path === '/' && state.conectado == true){
         Swal.fire("Ya estas conectado!")
@@ -77,7 +138,6 @@ router.beforeEach((to,from,next) => {
           }
           }).then((result) => {
               next('/')
-          /* Read more about handling dismissals below */
           if (result.dismiss === Swal.DismissReason.timer) {
               console.log('Fui cerrado por el timer')
           }
@@ -88,42 +148,6 @@ router.beforeEach((to,from,next) => {
       }
   }
 });
-
-  // if (to.path === '/' || to.path === '/register') {
-  //   if (to.path === '/') {
-  //     localStorage.getItem('connect');
-  //     localStorage.setItem('connect',false);
-  //     state.conectado = false;
-  //   }
-  //   next();
-  // }
-  //   else if(state.conectado == false || state.conectado == null) {
-  //     let timerInterval
-  //                       Swal.fire({
-  //                       title: 'Debes conectarte primero!',
-  //                       html: 'Seras redireccionado en <b></b> segundos.',
-  //                       timer: 3500,
-  //                       timerProgressBar: true,
-  //                       didOpen: () => {
-  //                           Swal.showLoading()
-  //                           const b = Swal.getHtmlContainer().querySelector('b')
-  //                           timerInterval = setInterval(() => {
-  //                           b.textContent = (Swal.getTimerLeft()/1000).toFixed(0)
-  //                           }, 100)
-  //                       },
-  //                       willClose: () => {
-  //                           clearInterval(timerInterval)
-  //                       }
-  //                       }).then((result) => {
-  //                           next('/')
-  //                       /* Read more about handling dismissals below */
-  //                       if (result.dismiss === Swal.DismissReason.timer) {
-  //                           console.log('Fui cerrado por el timer')
-  //                       }
-  //                       })
-  //   } else{
-  //     next();
-  // }
-});
+}); */
 
 export default router
