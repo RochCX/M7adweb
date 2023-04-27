b3cc0349455f0eb9d0e09c832a4d8417d5b2c807
ManuDv <m.alfaro@live.cl>
2023-04-27 12:56:45 -0400

    Arregla redireccionamiento next y animacion de flop

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,13 @@ import LoginView from '../views/LoginView.vue'
 import HomeView from '../views/HomeView.vue'
 import EditarCursos from '../views/EditarCursos.vue'
 import store from "@/store";
-// import Swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const auth = getAuth();
 
 const state = store.state;
+
 
 const routes = [
   {
@@ -41,46 +42,61 @@ const router = createRouter({
 
 
 router.beforeEach((to,from,next) => {
-  console.log(state.conectado)
+
+  let estadoConexion = eval(localStorage.getItem("conexion"))
 
   onAuthStateChanged(auth, (user) => {
     if (user) {
-      state.conectado = true;
+      state.conectado = true
+      localStorage.setItem("conexion", state.conectado);
       state.fireMail = user.email;
+      return
     } else {
       state.conectado = false;
+      state.fireMail = null;
+      localStorage.setItem("conexion", state.conectado);
+      return
     }
   })
 
-  if(!state.conectado) {
+  // Ya esta logueado
+  if(estadoConexion && to.path == '/') {
+    Swal.fire("¡Ya estás conectado!")
+        next('/home')
+        return
+  }
+
+  // Debes conectarte primero
+  if(!estadoConexion) {
     if (to.path == '/register') {
       next()
       return
     } else if (to.path != '/') {
-      console.log('primer next')
-      next('/')
+      let timerInterval
+          Swal.fire({
+          title: 'Debes conectarte primero!',
+          html: 'Serás redireccionado en <b></b> segundos.',
+          timer: 3500,
+          timerProgressBar: true,
+          didOpen: () => {
+              Swal.showLoading()
+              const b = Swal.getHtmlContainer().querySelector('b')
+              timerInterval = setInterval(() => {
+              b.textContent = (Swal.getTimerLeft()/1000).toFixed(0)
+              }, 100)
+          },
+          willClose: () => {
+              clearInterval(timerInterval)
+          }
+          }).then((result) => {
+              next('/')
+          if (result.dismiss === Swal.DismissReason.timer) {
+              console.log('Fui cerrado por el timer')
+          }
+          })
       return
-    }
+    } 
   } 
-  else {
-    if (to.path == '/') {
-      alert('Ya estas logueado men')
-      next()
-      return
-    }
-/*     onAuthStateChanged(auth, (user) => {
-      console.log(user)
-      if (user) {
-        state.conectado = true;
-        state.fireMail = user.email;
-        return
-      } else {
-        return
-      }
-    }) */
-  }
-
-  console.log('ultimo next')
   next();
 })
 
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -106,7 +106,6 @@ export default {
         footer: errorCode
       })
     });
-  console.log(this.correoIngresado, this.passIngresado)
 }
   }
 }
